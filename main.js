51470d3790821de28a88eda39edc0c1d4ebc90c3
yassinemessai <70016132+yassinemessai@users.noreply.github.com>
2020-08-26 16:23:56 +0100

    pushing work

---
+++ main.js
@@ -0,0 +1,48 @@
+var FavoriteTvshowsAndMovies["lucifer","casadelpapel","breakingbad","bodyguard","titanic"]
+var NamesOfPeopleIcareAbout ["mother","father","brother","sister"]
+var FavoriteSports[""]
+
+function sumFor(nums) {
+	var total = 0 ;
+	for (var i = 0; i<nums.length; i++) {
+		total = total + nums [i]
+	} 
+	return total ; 
+
+}
+function sumWhile(nums) {
+	var total = 0 ; 
+	var i = 0 ; 
+	while (i < nums.length) {
+    total = total + nums [i] ; 
+    i++ ; 
+
+
+	}
+	return total ; 
+}
+
+function maxArrayFor(num) {
+	var max = 0 ; 
+for (var i = 0 ; i < num.length ; i++ )  
+{
+if (max < num[i]) {
+
+}
+
+}
+return max 
+
+
+}
+
+
+
+
+}
+
+
+
+
+
+
